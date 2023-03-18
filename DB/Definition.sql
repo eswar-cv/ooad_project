1c149c62985240b9d8282e01d0b2c5de0d76b215
dhanushya1 <dhanushuma.2002@gmail.com>
2023-03-18 10:55:09 +0530

    DB-def files added

---
+++ DB/Definition.sql
@@ -0,0 +1,61 @@
+create database PROJECT ; 
+use PROJECT; 
+
+CREATE TABLE ACCOUNT 
+(
+name VARCHAR(20),
+USER_NAME VARCHAR(20) primary key, 
+password varchar(20), 
+user_type varchar(20) NOT NULL
+);
+CREATE TABLE COURSE
+(
+Course_id INT auto_increment PRIMARY KEY, 
+c_name varchar(20), 
+c_desc varchar(50)
+);
+
+CREATE TABLE ASSIGNMENT 
+(
+Assgn_id INT auto_increment primary key, 
+c_id INT, 
+deadline datetime, 
+assgn_file longblob, 
+instruc varchar(100), 
+foreign key(c_id) references COURSE(Course_id)
+);
+
+CREATE TABLE ACC_COURSE
+(
+S_name varchar(20), 
+c_id int, 
+u_type varchar(20), 
+primary key(S_name,c_id,u_type),
+foreign key(S_name) references account(user_name), 
+foreign key(c_id) references COURSE(Course_id));
+
+CREATE TABLE Stud_Teacher
+(
+stud_name varchar(20), 
+teach_name varchar(20),
+c_id int, 
+foreign key(stud_name) references account(user_name), 
+foreign key(teach_name) references account(user_name), 
+foreign key(c_id) references COURSE(Course_id)
+);
+
+CREATE TABLE Submission
+(
+stud_name varchar(20), 
+s_id int auto_increment primary key, 
+a_id int, 
+c_id int, 
+results int, 
+comments varchar(50), 
+remarks varchar(50), 
+sub_file longblob, 
+foreign key(stud_name) references account(user_name), 
+foreign key(c_id) references COURSE(Course_id), 
+foreign key(a_id) references ASSIGNMENT(assgn_id)
+);
+
